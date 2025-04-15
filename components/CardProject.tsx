6d3ba94848772f04459eb2ef1b2cf09a4b2a050e
Maria Castro <mcastrod08@gmail.com>
2025-04-15 17:14:19 +0200

    projects section icons

--- components/CardProject.tsx
+++ components/CardProject.tsx
@@ -4,7 +4,7 @@ import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 import MagicButton from "./ui/MagicButton";
 import { GiBottomRight3dArrow } from "react-icons/gi";
  
-export function CardProject({title, description, img, github, demo} : {title:string; description:string; img:string, github:string, demo:string}) {
+export function CardProject({title, description, img, github, demo, iconLists} : {title:string; description:string; img:string, github:string, demo:string, iconLists: string[]}) {
   return (
     <CardContainer className="inter-var">
       <CardBody className=" relative group/card w-auto sm:w-[30rem] h-auto rounded-xl p-6 border border-purple-500/[0.2] bg-[rgb(4,7,29)] " >
@@ -35,6 +35,27 @@ export function CardProject({title, description, img, github, demo} : {title:str
             alt="thumbnail"
           />
         </CardItem>
+        
+        <CardItem
+          translateZ="100"
+          rotateX={20}
+          rotateZ={-10}
+          className="w-full mt-6"
+        >
+        <div className="flex items-center justify-between mt.7 mb-3">
+          <div className="flex items-center">
+            {iconLists.map((icon, index) =>(
+              <div 
+                key={index}
+                className="border border-white/[0.2] rounded-full bg-black lg:w-10 w-8 h-8 flex justify-center items-center"
+              >
+                <img src={icon} alt={icon} className="p-2" />
+              </div>
+            ))}
+          </div>
+        </div>
+        </CardItem>
+
         <div className="flex justify-between items-center mt-20">
           
           <a href={demo} className="px-4 py-2 rounded-xl text-xs font-normal" target="_blank">
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -4,23 +4,21 @@ import { CardProject } from './CardProject'
 
 const RecentProjects = () => {
   return (
-    <div className="py-30">
-      <h1 className="heading mb-10"> Recent {" "}
-				<span className="text-purple-400">Projects</span>
-			</h1>
+    <div className="py-30 px-5">
+      	<h1 className="heading mb-10"> Recent {" "}
+			<span className="text-purple-400">Projects</span>
+		</h1>
 			{/* es-disable @typescript-eslint/no-unused-vars */}
-			<div className="mx-auto grid grid-cols-3 md:grid-cols-3 h-full w-full">
-				
-				{projects.map(({id, title, des, img, github, demo }) => (
-					<div
-						key={id}
-						className=" h-full flex items-center justify-center "
-					>
-						
-						<CardProject title={title} description={des} img={img} github={github} demo={demo}/>
-					</div>
-				))}
-			</div>
+		<div className="mx-auto grid lg:grid-cols-3 md:grid-cols-2  md:gap-4 sm:grid-cols-1 h-full w-full">
+			{projects.map(({id, title, des, img, github, demo, iconLists }) => (
+				<div
+					key={id}
+					className="lg:col-span-1 md:col-span-1 sm:col-span-1 xs:col-span-1"
+				>
+					<CardProject title={title} description={des} img={img} github={github} demo={demo} iconLists={iconLists}/>
+				</div>
+			))}
+		</div>
     </div>
   )
 }
