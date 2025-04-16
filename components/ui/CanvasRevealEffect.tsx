e0e44c8e54db01f4e29b3ce36217dc5bf54dd73a
Maria Castro <mcastrod08@gmail.com>
2025-04-16 12:54:38 +0200

    fix deploy

--- components/ui/CanvasRevealEffect.tsx
+++ components/ui/CanvasRevealEffect.tsx
@@ -4,6 +4,7 @@ import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import React, { useMemo, useRef } from "react";
 import * as THREE from "three";
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export const CanvasRevealEffect = ({
   animationSpeed = 0.4,
   opacities = [0.3, 0.3, 0.3, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8, 1],
