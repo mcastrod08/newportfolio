31a6dea6ebc1bc77fe8fcef4d8d4fda726a60e44
Maria Castro <mcastrod08@gmail.com>
2025-04-30 13:54:16 +0200

    fix design

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -9,11 +9,11 @@ const RecentProjects = () => {
 			<span className="bg-gradient-to-r from-white to-indigo-600 bg-clip-text text-transparent">Projects</span>
 		</h1>
 			{/* es-disable @typescript-eslint/no-unused-vars */}
-		<div className="flex lg:justify-between flex-wrap sm:justify-center pt-10">
+		<div className="flex justify-center flex-wrap pt-10 gap-4">
 			{projects.map(({id, title, des, img, github, demo, iconLists }) => (
 				<div
 					key={id}
-					className="lg:col-span-1 md:col-span-1 sm:col-span-1 xs:col-span-1"
+					className="lg:col-span-1 md:col-span-1 sm:col-span-1 xs:col-span-1 lg:px-10"
 				>
 					<CardProject title={title} description={des} img={img} github={github} demo={demo} iconLists={iconLists}/>
 				</div>
